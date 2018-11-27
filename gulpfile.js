4c46f5b8b1fbcd2b113f5fdafc675cc73eb794ef
linchengzzz <1139489076@qq.com>
2018-11-27 11:53:19 +0800

    完成压缩

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,17 @@
 var gulp = require('gulp');
-var minifycss = require('gulp-clean-css');
+var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
 var imagemin = require('gulp-imagemin');
-var del = require('del');
-var runSequence = require('run-sequence');
-var Hexo = require('hexo');
-// 清除public文件夹
-gulp.task('clean', function() {
-    return del(['public/**/*']);
-});
-// 利用Hexo API 来生成博客内容， 效果和在命令行运行： hexo g 一样
-// generate html with 'hexo generate'
-var hexo = new Hexo(process.cwd(), {});
-gulp.task('generate', function(cb) {
-    hexo.init()
-        .then(function() {
-            return hexo.call('generate', {
-                watch: false,
-            });
-        })
-        .then(function() {
-            return hexo.exit();
-        })
-        .then(function() {
-            return cb();
-        })
-        .catch(function(err) {
-            console.log(err);
-            hexo.exit(err);
-            return cb(err);
-        });
-});
-// 压缩public目录下的所有css
+// 压缩css文件
 gulp.task('minify-css', function() {
     return gulp
         .src('./public/**/*.css')
-        .pipe(
-            minifycss({
-                compatibility: 'ie8',
-            }),
-        )
+        .pipe(minifycss())
         .pipe(gulp.dest('./public'));
 });
-// 压缩public目录下的所有html
+// 压缩html文件
 gulp.task('minify-html', function() {
     return gulp
         .src('./public/**/*.html')
@@ -59,43 +26,25 @@ gulp.task('minify-html', function() {
         )
         .pipe(gulp.dest('./public'));
 });
-// 压缩public目录下的所有js
+// 压缩js文件
 gulp.task('minify-js', function() {
     return gulp
-        .src('./public/**/*.js')
+        .src(['./public/**/.js', '!./public/js/**/*min.js'])
         .pipe(uglify())
         .pipe(gulp.dest('./public'));
 });
-// 压缩public目录下的所有img： 这个采用默认配置
-gulp.task('minify-img', function() {
-    return gulp
-        .src('./public/images/**/*.*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./public/images'));
-});
-// 同上，压缩图片，这里采用了： 最大化压缩效果。
-gulp.task('minify-img-aggressive', function() {
-    return gulp
-        .src('./public/images/**/*.*')
+// 压缩 public/demo 目录内图片
+gulp.task('minify-images', function() {
+    gulp.src('./public/demo/**/*.*')
         .pipe(
-            imagemin(
-                [
-                    imagemin.gifsicle({ optimizationLevel: 3 }),
-                    imagemin.jpegtran({ progressive: true }),
-                    imagemin.optipng({ optimizationLevel: 7 }),
-                    imagemin.svgo(),
-                ],
-                { verbose: true },
-            ),
+            imagemin({
+                optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+                progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+                interlaced: false, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+                multipass: false, //类型：Boolean 默认：false 多次优化svg直到完全优化
+            }),
         )
-        .pipe(gulp.dest('./public/images'));
+        .pipe(gulp.dest('./public/uploads'));
 });
-// 用run-sequence并发执行，同时处理html，css，js，img
-gulp.task('compress', function(cb) {
-    runSequence(['minify-html', 'minify-css', 'minify-js', 'minify-img-aggressive'], cb);
-});
-// 执行顺序： 清除public目录 -> 产生原始博客内容 -> 执行压缩混淆
-gulp.task('build', function(cb) {
-    runSequence('clean', 'generate', 'compress', cb);
-});
-gulp.task('default', ['build']);
+// 默认任务
+gulp.task('default', ['minify-html', 'minify-css', 'minify-js', 'minify-images']);
--- update.sh
+++ update.sh
@@ -7,3 +7,7 @@ read -p "请输入要更新的内容：" commit
 git add .
 git commit -m${commit}
 git push origin master
+hexo g
+gulp
+hexo d
+echo -e "\033[32m 博客文章更新成功，请远程部署 !!! \033[0m"
